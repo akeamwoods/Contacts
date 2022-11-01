f0755a5ed676aab98124c01e03cc9f24fa9e1669
Akeam Woods <a19941@hotmail.co.uk>
2022-11-01 13:41:22 +0000

    adds react-query (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import Contact from './types/Contact';
+import { useQuery } from 'react-query';
+
+const getData: () => Promise<Contact[]> = async () =>
+	await (await fetch('/api/contacts.json')).json();
+
 const App: React.FC = () => {
-	return <div>hello</div>;
+	const { isLoading, data } = useQuery<Contact[]>(
+		'contacts',
+		async () => await getData()
+	);
+
+	if (isLoading) return <div>Loading...</div>;
+
+	if (data == null) return <div>No data...</div>;
+
+	return (
+		<div>
+			{data.map((d) => (
+				<div key={d.email}>{d.email}</div>
+			))}
+		</div>
+	);
 };
 
 export default App;
